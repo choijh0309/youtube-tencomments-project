bc869034ee6d74f58aec1db95cb65f60b83dad9c
JUNHA CHOI <choijh0309@naver.com>
2024-09-01 14:48:42 +0900

    Test: YoutubeCommentProjectApplication

---
+++ src/test/java/me/choijunha/youtubecommentproject/YoutubeCommentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choijunha.youtubecommentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCommentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
